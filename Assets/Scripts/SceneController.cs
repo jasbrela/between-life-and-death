bd62d41bd8b72a7d6e2690a76760b4f9b5cd4547
Clara Romeiro <clararomeirocosta@gmail.com>
2021-10-10 17:01:21 -0300

    toggle music and sfx

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SceneController : MonoBehaviour
 {
     [Header("Nome da cena de gameplay")]
     [SerializeField] private string startGameSceneName;
 
+    [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private AudioSource audioSource;
+
     public void StartGame()
     {
         Reset();
@@ -37,6 +41,30 @@ public class SceneController : MonoBehaviour
         else
         {
             panelCredits.SetActive(false);
+        }
+    }
+
+    public void musicToggleClick(Toggle music)
+    {
+        if (!music.isOn)
+        {
+            audioMixer.SetFloat("Music", -88);
+        }
+        else
+        {
+            audioMixer.SetFloat("Music", 0);
+        }
+    }
+
+    public void sfxToggleClick(Toggle sfx)
+    {
+        if (!sfx.isOn)
+        {
+            audioMixer.SetFloat("SFX", -88);
+        }
+        else
+        {
+            audioMixer.SetFloat("SFX", 0);
         }
     }
 }
