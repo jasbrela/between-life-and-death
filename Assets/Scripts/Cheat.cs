114ec76c1f3f58168457dc916f29509afa34a708
Gabriela Milet <jasbrela@gmail.com>
2021-10-11 00:16:55 -0300

    fix starting with skin

--- Assets/Scripts/Cheat.cs
+++ Assets/Scripts/Cheat.cs
@@ -7,9 +7,15 @@ public class Cheat : MonoBehaviour
 #if UNITY_EDITOR
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.F1))
         {
             PlayerPrefs.SetInt("allCandies", PlayerPrefs.GetInt("allCandies") + 100);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.Save();
         }
     }
 #endif
